ff17ef2712aee350b8cbf520cdc4ebd1ee1f62cd
Edu Melgaço <eduhdm0105@gmail.com>
2022-06-17 23:36:47 -0300

    De volta ao teste funcional - PARTE 1

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -32,51 +32,39 @@ class NewListTest(TestCase):
         new_list = List.objects.first()
         self.assertRedirects(response, f'/lists/{new_list.id}/')
 
+class ListViewTest(TestCase):
+    def test_uses_list_template(self):
+        my_list = List.objects.create()
+        response = self.client.get(f'/lists/{my_list.id}/')
+        self.assertTemplateUsed(response, 'list.html')
 
-class ListAndItemModelsTest(TestCase):
+    def test_displays_only_items_for_that_list(self):
+        correct_list = List.objects.create()
+        Item.objects.create(text='itemey 1', list=correct_list)
+        Item.objects.create(text='itemey 2', list=correct_list)
+        other_list = List.objects.create()
+        Item.objects.create(text='other list item 1', list=other_list)
+        Item.objects.create(text='other list item 2', list=other_list)
 
-    def test_saving_and_retrieving_items(self):
-        my_list = List()
-        my_list.save()
+        response = self.client.get(f'/lists/{correct_list.id}/')
 
-        first_item = Item()
-        first_item.text = 'O primeiro item'
-        first_item.list = my_list
-        first_item.save()
+        self.assertContains(response, 'itemey 1')
+        self.assertContains(response, 'itemey 2')
+        self.assertNotContains(response, 'other list item 1')
+        self.assertNotContains(response, 'other list item 2')
 
-        second_item = Item()
-        second_item.text = 'O segundo item'
-        second_item.list = my_list
-        second_item.save()
-
-        saved_list = List.objects.first()
-        self.assertEqual(saved_list, my_list)
-
-        saved_items = Item.objects.all()
-        self.assertEqual(saved_items.count(), 2)
-
-        first_saved_item = saved_items[0]
-        second_saved_item = saved_items[1]
-        self.assertEqual(first_saved_item.text, 'O primeiro item')
-        self.assertEqual(first_saved_item.list, my_list)
-        self.assertEqual(second_saved_item.text, 'O segundo item')
-        self.assertEqual(second_saved_item.list, my_list)
-
-class NewItemTest(TestCase):
-
-    def test_validation_errors_are_sent_back_to_home_page_template(self):
-        response = self.client.post('/lists/new', data={'item_text': ''})
-        self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'home.html')
-        expected_error = escape("You can't have an empty list item")
-        self.assertContains(response, expected_error)
+    def test_passes_correct_list_to_template(self):
+        other_list = List.objects.create()
+        correct_list = List.objects.create()
+        response = self.client.get(f'/lists/{correct_list.id}/')
+        self.assertEqual(response.context['list'], correct_list)
 
     def test_can_save_a_POST_request_to_an_existing_list(self):
         other_list = List.objects.create()
         correct_list = List.objects.create()
 
         self.client.post(
-            f'/lists/{correct_list.id}/add_item',
+            f'/lists/{correct_list.id}/',
             data={'item_text': 'A new item for an existing list'}
         )
 
@@ -85,14 +73,12 @@ class NewItemTest(TestCase):
         self.assertEqual(new_item.text, 'A new item for an existing list')
         self.assertEqual(new_item.list, correct_list)
 
-
     def test_redirects_to_list_view(self):
         other_list = List.objects.create()
         correct_list = List.objects.create()
 
         response = self.client.post(
-            f'/lists/{correct_list.id}/add_item',
+            f'/lists/{correct_list.id}/',
             data={'item_text': 'A new item for an existing list'}
         )
-
         self.assertRedirects(response, f'/lists/{correct_list.id}/')
